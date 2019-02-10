e64ffa2999a6e00f65e0058e496110412602d62b
givemin5 <givemin5@gmail.com>
2019-02-10 20:44:53 +0800

    Add Test

--- LeetCode832FlippingAnImage/UnitTest1.cs
+++ LeetCode832FlippingAnImage/UnitTest1.cs
@@ -9,6 +9,26 @@ namespace LeetCode832FlippingAnImage
         [TestMethod]
         public void TestMethod1()
         {
+            int[][] A = new int[][]
+            {
+                new int[]{1, 1, 0},
+                new int[]{1, 0, 1},
+                new int[]{0, 0, 0}
+            };
+
+            Solution solution = new Solution();
+
+            var actual = solution.FlipAndInvertImage(A);
+
+            var expected = new int[][]
+            {
+                new int[]{1, 0, 0},
+                new int[]{0, 1, 0},
+                new int[]{1, 1, 1}
+            };
+
+            CollectionAssert.AreEqual(expected,actual);
+            
         }
     }
 }
